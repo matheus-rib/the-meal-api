bb3bf99ed690fd3e8bff1599965ec2865d1b115e
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2021-01-27 16:19:40 -0300

    gs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express'
+import packageJson from '../../package.json'
 
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.json('hello world')
+  res.json({
+    name: packageJson.name,
+    version: packageJson.version,
+    author: packageJson.author,
+    license: packageJson.license,
+    repository: packageJson.repository.url,
+  })
 })
 
 export default router
