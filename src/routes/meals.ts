5a7cab08d42c5153a927bcab627bd795c8e093f2
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2021-01-27 17:20:07 -0300

    fixed type in filename

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -2,14 +2,14 @@ import { Router } from 'express'
 import requestHandler from '../utils/requestHandler'
 
 // Controllers
-import MealsControllers from '../controllers/MealsControllers'
+import MealsController from '../controllers/MealsController'
 
 const router = Router()
 
-router.get('/categories', requestHandler(MealsControllers.categoriesList))
-router.get('/categories/:categoryName', requestHandler(MealsControllers.mealsInCategoryList))
-router.get('/show/:mealId', requestHandler(MealsControllers.show))
-router.get('/search', requestHandler(MealsControllers.search))
-router.get('/random', requestHandler(MealsControllers.random))
+router.get('/categories', requestHandler(MealsController.categoriesList))
+router.get('/categories/:categoryName', requestHandler(MealsController.mealsInCategoryList))
+router.get('/show/:mealId', requestHandler(MealsController.show))
+router.get('/search', requestHandler(MealsController.search))
+router.get('/random', requestHandler(MealsController.random))
 
 export default router
